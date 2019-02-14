15fcbda2315e9bb23826e31e94ae2fd2c8d79a06
abel <abel.wang@microsoft.com>
2019-02-14 13:47:09 -0800

    added change to live demo. What could go wrong?

--- src/Nav.js
+++ src/Nav.js
@@ -2,7 +2,7 @@ import React from "react";
 
 const Nav = () => (
   <nav>
-    <h1>TAILWIND TRADERS - Welcome to Ready 2019</h1>
+    <h1>TAILWIND TRADERS - Welcome to Ready 2019  - Made A Change For Live Demo!!</h1>
   </nav>
 );
 
