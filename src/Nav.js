8edf7eab6795a64e1c7bd205c9d427e6520afce6
abel <abel.wang@microsoft.com>
2019-03-11 10:47:29 -0700

    made change for demo, fixes AB#65

--- src/Nav.js
+++ src/Nav.js
@@ -2,7 +2,7 @@ import React from "react";
 
 const Nav = () => (
   <nav>
-    <h1>TAILWIND TRADERS - Welcome to Ready 2019 - Made A Change For Live Demo</h1>
+    <h1>Make some changes TAILWIND TRADERS - Welcome to Ready 2019 - Made A Change For Live Demo</h1>
   </nav>
 );
 
